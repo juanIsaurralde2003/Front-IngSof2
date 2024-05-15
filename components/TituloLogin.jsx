7b2c12f1a5a3584fce68edeb60cb7cd5a2d9d573
Juan Isaurralde <jisaurralde@correo.um.edu.uy>
2024-05-15 09:40:49 -0300

    se borra el push token luego al cerrar sesión, se hace response received handler para las push tipo daily-prompts, diseño en titulo login

--- components/TituloLogin.jsx
+++ components/TituloLogin.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { StyleSheet,View,Text,Image} from "react-native";
 import StyledText from "../components/StyledText";
+import { CameraIcon } from "./CameraIcon";
 
 
 function TituloLogin(props){
     return(
         <View style = {styles.container}>
             <View style={styles.beRealContainer}>
-                <StyledText titleText>
-                    Don't Be Real                    
-                </StyledText>
+                <Text style={styles.headerText}>
+                    <Text style={styles.dontText}>DON'T</Text>
+                    <Text style={styles.beRealText}> BE</Text>
+                    <Text style={styles.beRealText}> REAL</Text>
+                    
+                </Text>
+            </View>
+            <View style={styles.cameraContainer}>
+                <CameraIcon/>
             </View>   
         </View>
     )
@@ -20,7 +27,8 @@ const styles = StyleSheet.create({
         flex:1,
         flexDirection:'row',
         marginTop:190,
-        justifyContent:'center'
+        justifyContent:'center',
+        marginLeft:20
     },
     beRealContainer:{
         alignItems:'center',
@@ -32,6 +40,24 @@ const styles = StyleSheet.create({
         height: 51,
         flex:1,
       },
+      headerText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        //fontFamily: 'NotoSansTC-Regular', 
+      },
+      dontText: {
+        color: '#000',
+      },
+      beRealText: {
+        color: '#390294', 
+      },
+      cameraContainer:{
+        margin:10
+      },
+      cameraIcon:{
+        height:50,
+        width:50
+      }
 })
 
 export default TituloLogin;
